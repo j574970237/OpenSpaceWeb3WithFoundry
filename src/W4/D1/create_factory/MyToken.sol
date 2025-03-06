c4e82586550dac5158ea8d575242ff0e99cba4d9
JackZhu <574970237@qq.com>
2025-03-06 17:04:32 +0800

    fix: 通过initializer修饰符确保仅调用一次

--- src/W4/D1/create_factory/MyToken.sol
+++ src/W4/D1/create_factory/MyToken.sol
@@ -6,11 +6,9 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 contract MyToken is Initializable, ERC20Upgradeable {
     uint256 public perMint;
-    bool private init_flag;
+
+    // 通过initializer修饰符确保仅调用一次
     function initialize(string memory name, string memory symbol, uint256 initialSupply, uint256 _perMint) public initializer {
-        // 控制只能调用一次，多次调用只会重置当前合约，并不会产生多个合约
-        require(!init_flag, "Already init!");
-        init_flag = true;
         __ERC20_init(name, symbol);
         _mint(msg.sender, initialSupply * 1e18);
         perMint = _perMint;
