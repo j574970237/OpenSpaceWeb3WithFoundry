24337d5edf3f6eb9f8a18ca5110e6e9c3e82a9af
JackZhu <574970237@qq.com>
2025-03-06 10:53:38 +0800

    fix: 控制最小代理initialize方法只能调用一次

--- src/W4/D1/create_factory/MyToken.sol
+++ src/W4/D1/create_factory/MyToken.sol
@@ -6,7 +6,11 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 contract MyToken is Initializable, ERC20Upgradeable {
     uint256 public perMint;
+    bool private init_flag;
     function initialize(string memory name, string memory symbol, uint256 initialSupply, uint256 _perMint) public initializer {
+        // 控制只能调用一次，多次调用只会重置当前合约，并不会产生多个合约
+        require(!init_flag, "Already init!");
+        init_flag = true;
         __ERC20_init(name, symbol);
         _mint(msg.sender, initialSupply * 1e18);
         perMint = _perMint;
